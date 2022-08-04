83f88f2abea572e8a1458eb83454ab9f0ae26e1c
AndrewwwDev <98230530+AndrewwwDev@users.noreply.github.com>
2022-08-04 12:00:16 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  urlServer: 'https://form-200-default-rtdb.firebaseio.com'
+  urlServer: 'https://prueba-10beb-default-rtdb.firebaseio.com/'
+  //urlServer: 'https://form-200-default-rtdb.firebaseio.com'
 };
